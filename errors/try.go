0e8c6acdf3a98979b834764321bbd65da000c570
liyong <liyong@lanren-inc.com>
2024-05-10 17:19:18 +0800

    Lmicroseconds

--- errors/try.go
+++ errors/try.go
@@ -5,6 +5,9 @@ import (
 	"github.com/remoting/frame/reflect"
 )
 
+func TryCatch(tryFunc func() error, catchFunc func(err error)) {
+	catchFunc(TryError(tryFunc))
+}
 func TryError(tryFunc func() error) (err error) {
 	defer func() {
 		if r := recover(); r != nil {
@@ -13,7 +16,7 @@ func TryError(tryFunc func() error) (err error) {
 	}()
 	return tryFunc()
 }
-func TryCatch[T any](tryFunc func() (T, error)) (t T, err error) {
+func TryAny[T any](tryFunc func() (T, error)) (t T, err error) {
 	defer func() {
 		if r := recover(); r != nil {
 			err = fmt.Errorf("%v", r)
