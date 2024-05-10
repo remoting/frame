9e289abc81d142c198eb0fed85b249fca5bd1b0d
liyong <liyong@lanren-inc.com>
2024-05-10 18:49:36 +0800

    Recover

--- errors/try.go
+++ errors/try.go
@@ -5,23 +5,22 @@ import (
 	"github.com/remoting/frame/reflect"
 )
 
+func Recover(err *error) {
+	if p := recover(); p != nil {
+		*err = fmt.Errorf("%v", p)
+	} else {
+		*err = nil
+	}
+}
 func TryCatch(tryFunc func() error, catchFunc func(err error)) {
 	catchFunc(TryError(tryFunc))
 }
 func TryError(tryFunc func() error) (err error) {
-	defer func() {
-		if r := recover(); r != nil {
-			err = fmt.Errorf("%v", r)
-		}
-	}()
+	defer Recover(&err)
 	return tryFunc()
 }
 func TryAny[T any](tryFunc func() (T, error)) (t T, err error) {
-	defer func() {
-		if r := recover(); r != nil {
-			err = fmt.Errorf("%v", r)
-		}
-	}()
+	defer Recover(&err)
 	return tryFunc()
 }
 func TryThrow(err error) {
