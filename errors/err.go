c0e92ed9af4ba2f38cbc7e02ec1c6eed17bb71b9
liyong <liyong@lanren-inc.com>
2024-05-11 13:27:29 +0800

    Recover

--- errors/err.go
+++ errors/err.go
@@ -1,6 +1,9 @@
 package errors
 
-import "errors"
+import (
+	"errors"
+	pkgErrors "github.com/pkg/errors"
+)
 
 type RestError struct {
 	error
@@ -9,7 +12,7 @@ type RestError struct {
 
 func NewRestError(code int, msg string) RestError {
 	return RestError{
-		error: errors.New(msg),
+		error: New(msg),
 		Code:  code,
 	}
 }
@@ -17,12 +20,39 @@ func NewRestError(code int, msg string) RestError {
 func Is(err, target error) bool {
 	return errors.Is(err, target)
 }
-func New(text string) error {
-	return errors.New(text)
-}
 func As(err, target error) bool {
 	return errors.As(err, target)
 }
 func Unwrap(err error) error {
 	return errors.Unwrap(err)
+}
+
+// ////////
+func New(message string) error {
+	return pkgErrors.New(message)
+}
+
+func Errorf(format string, args ...interface{}) error {
+	return pkgErrors.Errorf(format, args)
+}
+func WithStack(err error) error {
+	return pkgErrors.WithStack(err)
+}
+func Wrap(err error, message string) error {
+	return pkgErrors.Wrap(err, message)
+}
+
+func Wrapf(err error, format string, args ...interface{}) error {
+	return pkgErrors.Wrapf(err, format, args)
+}
+
+func WithMessage(err error, message string) error {
+	return pkgErrors.WithMessage(err, message)
+}
+
+func WithMessagef(err error, format string, args ...interface{}) error {
+	return pkgErrors.WithMessagef(err, format, args)
+}
+func Cause(err error) error {
+	return pkgErrors.Cause(err)
 }
--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/gin-gonic/gin v1.9.0
 	github.com/natefinch/lumberjack v2.0.0+incompatible
+	github.com/pkg/errors v0.9.1
 	golang.org/x/crypto v0.8.0
 	gopkg.in/gomail.v2 v2.0.0-20160411212932-81ebce5c23df
 	gorm.io/driver/mysql v1.5.1
