fe5e5a2bc054d536e3b86302a0eb6383145f9075
liyong <liyong@lanren-inc.com>
2024-07-10 14:58:09 +0800

    Recover

--- go.mod
+++ go.mod
@@ -6,7 +6,6 @@ require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/gin-contrib/gzip v1.0.1
 	github.com/gin-gonic/gin v1.9.1
-	github.com/natefinch/lumberjack v2.0.0+incompatible
 	github.com/pkg/errors v0.9.1
 	golang.org/x/crypto v0.22.0
 	gopkg.in/gomail.v2 v2.0.0-20160411212932-81ebce5c23df
--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -7,8 +7,6 @@ import (
 	"log"
 	"os"
 	"runtime"
-
-	"github.com/natefinch/lumberjack"
 )
 
 var errorLog, infoLog, warnLog *log.Logger
@@ -20,6 +18,7 @@ type Config struct {
 	Level      int
 	MaxSize    int
 	MaxBackups int
+	MaxAge     int
 }
 
 // InitConfig ...
@@ -34,29 +33,35 @@ func InitConfig(conf Config) io.Writer {
 	if conf.MaxBackups <= 0 {
 		conf.MaxBackups = 3
 	}
+	if conf.MaxSize <= 0 {
+		conf.MaxSize = 10
+	}
+	if conf.MaxAge <= 0 {
+		conf.MaxAge = 7
+	}
 	_level = conf.Level
-	errorLogFile := &lumberjack.Logger{
+	errorLogFile := &Logger{
 		Filename:   conf.LogDir + "error.log",
 		MaxSize:    conf.MaxSize, // megabytes
 		MaxBackups: conf.MaxBackups,
-		MaxAge:     28, // days
+		MaxAge:     conf.MaxAge, // days
 	}
 	errorLog = log.New(io.MultiWriter(os.Stdout, errorLogFile), "["+conf.Prefix+"] [ERROR] ", log.Ldate|log.Lmicroseconds|log.Llongfile)
 
-	infoLogFile := &lumberjack.Logger{
+	infoLogFile := &Logger{
 		Filename:   conf.LogDir + "info.log",
 		MaxSize:    conf.MaxSize, // megabytes
 		MaxBackups: conf.MaxBackups,
-		MaxAge:     28, // days
+		MaxAge:     conf.MaxAge, // days
 	}
 	writer := io.MultiWriter(os.Stdout, infoLogFile)
 	infoLog = log.New(writer, "["+conf.Prefix+"] [INFO] ", log.Ldate|log.Lmicroseconds|log.Llongfile)
 
-	warnLogFile := &lumberjack.Logger{
+	warnLogFile := &Logger{
 		Filename:   conf.LogDir + "warn.log",
 		MaxSize:    conf.MaxSize, // megabytes
 		MaxBackups: conf.MaxBackups,
-		MaxAge:     28, // days
+		MaxAge:     conf.MaxAge, // days
 	}
 	warnLog = log.New(io.MultiWriter(os.Stdout, warnLogFile), "["+conf.Prefix+"] [WARN] ", log.Ldate|log.Lmicroseconds|log.Llongfile)
 
