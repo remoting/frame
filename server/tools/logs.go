97581b535a5c8f72ed679bcc32d89fb0571a5528
liyong <liyong@lanren-inc.com>
2024-07-12 13:08:54 +0800

    exec

--- server/tools/logs.go
+++ server/tools/logs.go
@@ -9,15 +9,15 @@ import (
 func Logger() gin.HandlerFunc {
 	return gin.LoggerWithFormatter(func(param gin.LogFormatterParams) string {
 		// 你的自定义格式
-		return fmt.Sprintf("[%s] [ACCESS] [%s] %d %s %s %s %s %s %s %s\n",
+		return fmt.Sprintf("[%s] [ACCESS] [%s] %s %s %d %s %s %s %s %s\n",
 			logger.Conf.Prefix,
 			param.TimeStamp.Format("2006-01-02 15:04:05"),
-			param.StatusCode,
 			param.ClientIP,
-			param.Method,
-			param.Path,
 			param.Request.Proto,
+			param.StatusCode,
+			param.Method,
 			param.Latency,
+			param.Request.URL,
 			param.Request.UserAgent(),
 			param.ErrorMessage,
 		)
