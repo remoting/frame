8ec976e54b5e5c7d1db5efa0d163a1f853c32199
liyong <liyong@lanren-inc.com>
2024-07-15 18:55:05 +0800

    exec

--- pkg/goroutine/goroutine.go
+++ pkg/goroutine/goroutine.go
@@ -3,13 +3,19 @@ package goroutine
 import (
 	"fmt"
 	"github.com/remoting/frame/pkg/logger"
-	"runtime/debug"
 	"time"
 )
 
-func Recover() {
+func Recover(fn ...func(err error)) {
 	if p := recover(); p != nil {
-		logger.Error("error=%s", fmt.Errorf("%+v\n%s", p, debug.Stack()).Error())
+		err := fmt.Errorf("%+v\n", p)
+		if len(fn) > 0 {
+			for _, call := range fn {
+				call(err)
+			}
+		} else {
+			logger.Error("error=%s", err.Error())
+		}
 	}
 }
 
@@ -27,12 +33,10 @@ func SafeRetry(fn func()) {
 func retryRun(fn func()) {
 	for {
 		func() {
-			defer func() {
-				if r := recover(); r != nil {
-					logger.Error("Recovered from panic: %v. Retrying in 0.5 seconds...\n", r)
-					time.Sleep(500 * time.Millisecond)
-				}
-			}()
+			defer Recover(func(err error) {
+				logger.Error("Recovered from panic: %v. Retrying in 0.5 seconds...\n", err)
+				time.Sleep(500 * time.Millisecond)
+			})
 			fn()
 		}()
 	}
