bd8239afe2b4f2396b7ce284a0964df568774a9b
remoting <remoting@qq.com>
2024-07-20 19:02:47 +0800

    2.0

---
+++ pkg/lock/lock.go
@@ -0,0 +1,23 @@
+package lock
+
+import "sync"
+
+var _keyLock = NewKeyLock()
+
+type KeyLock struct {
+	mutexes sync.Map
+}
+
+func NewKeyLock() *KeyLock {
+	return &KeyLock{}
+}
+func Lock(key string) func() {
+	return _keyLock.Lock(key)
+}
+func (m *KeyLock) Lock(key string) func() {
+	value, _ := m.mutexes.LoadOrStore(key, &sync.Mutex{})
+	mtx := value.(*sync.Mutex)
+	mtx.Lock()
+
+	return func() { mtx.Unlock() }
+}
