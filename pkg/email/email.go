5ea7c68c6d953046b7c57684133dd965cc6466da
liyong <liyong@lanren-inc.com>
2024-08-12 11:49:41 +0800

    http

--- pkg/email/email.go
+++ pkg/email/email.go
@@ -13,11 +13,12 @@ type Address struct {
 }
 
 type SmtpMail struct {
-	Host string
-	Port int
-	User string
-	Pass string
-	From *Address
+	Host   string
+	Port   int
+	User   string
+	Pass   string
+	From   *Address
+	Dialer *gomail.Dialer
 }
 
 func (smtp *SmtpMail) Send(to *Address, subject, body string) error {
@@ -26,21 +27,22 @@ func (smtp *SmtpMail) Send(to *Address, subject, body string) error {
 	m.SetHeader("To", m.FormatAddress(to.Address, to.Name))
 	m.SetHeader("Subject", subject)
 	m.SetBody("text/html", body)
-	d := gomail.NewDialer(smtp.Host, smtp.Port, smtp.User, smtp.Pass)
-	return d.DialAndSend(m)
+	return smtp.Dialer.DialAndSend(m)
 }
 
-func OnInit(host, user, pass, address, name string, port int) {
+func OnInit(host, user, pass, address, name string, port int) *SmtpMail {
 	_smtpMail = &SmtpMail{
 		Host: host,
+		Port: port,
 		User: user,
 		Pass: pass,
-		Port: port,
 		From: &Address{
 			Name:    name,
 			Address: address,
 		},
 	}
+	_smtpMail.Dialer = gomail.NewDialer(_smtpMail.Host, _smtpMail.Port, _smtpMail.User, _smtpMail.Pass)
+	return _smtpMail
 }
 func SendMail(to *Address, subject, body string) error {
 	if _smtpMail == nil {
